ceab3188a5228f1eeabc469390fcf64aa6e2c66f
Мамонтов Денис Александрович <217019@edu.fa.ru>
2024-12-27 11:36:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = 0, num2 = 0;
+        while (true) {
+            try {
+
+                System.out.println("Введите первое целое число: ");
+                num1 = Integer.parseInt(scanner.nextLine());
+
+                System.out.println("Введите второе целое число: ");
+                num2 = Integer.parseInt(scanner.nextLine());
+
+                System.out.println("Сумма: " + (num1 + num2));
+                break;
+            } catch (NumberFormatException e) {
+
+                System.out.println("Ошибка: Введите корректные целые числа.");
+            }
+        }
+        scanner.close();
+    }
+}
